97c0c4ccf8093abcd1afd3d1252a3a17acf9b52d
Yusuf Emre Altun <yusufemrealtun08@gmail.com>
2024-03-25 11:09:07 +0300

    feat(main): Easy localisations test codes added to Main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,59 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture_template/product/init/language/locale_keys.g.dart';
+import 'package:flutter_architecture_template/product/init/product_localization.dart';
+import 'package:flutter_architecture_template/product/utility/enums/locales.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  
+  runApp(
+    ProductLocalization(child: MyApp()),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(LocaleKeys.home_title).tr(),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(LocaleKeys.hello).tr(),
+                  ElevatedButton(
+                    onPressed: () {
+                      ProductLocalization.updateLanguage(
+                        context: context,
+                        value: Locales.tr,
+                      );
+                    },
+                    child: Text('tr').tr(),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      ProductLocalization.updateLanguage(
+                        context: context,
+                        value: Locales.en,
+                      );
+                    },
+                    child: Text('en').tr(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
